d251adc75c46a4e13e1d76141ae5fe59cc557923
C04Le <bursiktomas@icloud.com>
2025-04-11 02:36:18 +0300

    update analysis in stats.py and printout in main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from stats import count_words
-from stats import count_characters
+from stats import character_dictionary_list
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     word_count = count_words(text)
-    characters = count_characters(text)
-    print(f"{word_count} words found in the document")
-    print(characters)
+    sorted_character_list = character_dictionary_list(text)
+    print(
+        "=" * 12 + " BOOKBOT " + "=" * 12 + 
+        "\nAnalyzing book found at books/frankenstein.txt..." +
+        "\n" + "-" * 11 + " Word Count " + "-" *10 +
+        "\n" f"{word_count} words found in the document" +
+        "\n" + "-" * 8 + " Character Count " + "-" * 8)
+    create_character_print(sorted_character_list)
+    print("=" * 14 + "END" + "=" * 14)
+
+
+# prints out characters and their values
+
+def create_character_print (sorted_character_list):
+    for character in sorted_character_list:
+        if character["letter"].isalpha():
+            print(f"{character["letter"]}: {character["count"]}")
 
 def get_book_text(filepath):
     with open(filepath) as file:
--- stats.py
+++ stats.py
@@ -2,6 +2,35 @@ def count_words(text):
     count = len(text.split())
     return count
 
+
+# defines how to sort list of dictionaries
+# sort based on the value of key 'count'
+
+def sort_on(dict):
+    return dict["count"]
+
+
+# create a sorted list of dictionaries
+# from a dictionary of all characters and their count
+# 2 key-value pairs > letter and count
+
+def character_dictionary_list(text):
+    character_dict = count_characters(text)
+    character_list = []
+
+    for key in character_dict.keys():
+        new_character_dict = {}
+        new_character_dict["letter"] = key
+        new_character_dict["count"] = character_dict[key]
+        character_list.append(new_character_dict)
+    
+    character_list.sort(reverse=True, key=sort_on)
+    return character_list
+
+
+# create a dictionary of all characters in text and their count
+# key = the character, value = count how many times it appears
+
 def count_characters(text):
     character_dictionary = {}
     text_characters = list(text.lower())
